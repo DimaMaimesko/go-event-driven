2ff7722bda68f1f11d16d4b694f3d81b1e60829c
Three Dots Labs <contact@threedotslabs.com>
2026-05-24 02:09:53 +0300

    completed 03-message-broker/02-publishing-messages

--- 03-message-broker/02-publishing-messages/main.go
+++ 03-message-broker/02-publishing-messages/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/redis/go-redis/v9"
+)
+
+const topic = "progress"
+
+func main() {
+	logger := watermill.NewSlogLogger(nil)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	pub, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	msg := message.NewMessage(watermill.NewUUID(), []byte("50"))
+	err = pub.Publish(topic, msg)
+	if err != nil {
+		panic(err)
+	}
+
+	msg = message.NewMessage(watermill.NewUUID(), []byte("100"))
+	err = pub.Publish(topic, msg)
+	if err != nil {
+		panic(err)
+	}
 }
