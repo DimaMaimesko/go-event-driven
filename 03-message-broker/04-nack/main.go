17656b12884f7693a4f557a41516ed89f4e3657f
Three Dots Labs <contact@threedotslabs.com>
2026-05-25 02:54:33 +0300

    completed 03-message-broker/04-nack

--- 03-message-broker/04-nack/main.go
+++ 03-message-broker/04-nack/main.go
@@ -18,6 +18,19 @@ func ConsumeMessages(sub message.Subscriber, alarmClient AlarmClient) {
 	}
 
 	for msg := range messages {
+		value := string(msg.Payload)
+
+		if value == "1" {
+			err = alarmClient.StartAlarm()
+		} else if value == "0" {
+			err = alarmClient.StopAlarm()
+		}
+
+		if err == nil {
+			msg.Ack()
+		} else {
+			msg.Nack()
+		}
 
 	}
 }
