a5640ab9f85c0cb5f1ba1423e5ecd27b6139b764
Three Dots Labs <contact@threedotslabs.com>
2026-05-25 21:11:40 +0300

    completed 04-router/02-no-publisher-handlers

--- 04-router/02-no-publisher-handlers/main.go
+++ 04-router/02-no-publisher-handlers/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	logger := watermill.NewSlogLogger(nil)
+
+	router := message.NewDefaultRouter(logger)
 
 	rdb := redis.NewClient(&redis.Options{
 		Addr: os.Getenv("REDIS_ADDR"),
@@ -21,4 +26,20 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	router.AddConsumerHandler(
+		"print-temperature",
+		"temperature-fahrenheit",
+		sub,
+		func(msg *message.Message) error {
+			fmt.Println("Temperature read:", string(msg.Payload))
+			return nil
+		},
+	)
+
+	err = router.Run(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
 }
