06cc1bad3a75d19eee1e6522f4c35f8d66ca7cd2
Three Dots Labs <contact@threedotslabs.com>
2026-05-27 00:27:17 +0300

    completed 05-events/04-project-tickets-status

--- project/http/handler_tickets.go
+++ project/http/handler_tickets.go
@@ -2,32 +2,40 @@ package http
 
 import (
 	"net/http"
+	"tickets/entities"
 
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/labstack/echo/v4"
 )
 
-type ticketsConfirmationRequest struct {
-	Tickets []string `json:"tickets"`
+type TicketsStatusRequest struct {
+	Tickets []TicketStatusRequest `json:"tickets"`
 }
 
-func (h Handler) PostTicketsConfirmation(c echo.Context) error {
-	var request ticketsConfirmationRequest
+type TicketStatusRequest struct {
+	TicketID      string         `json:"ticket_id"`
+	Status        string         `json:"status"`
+	Price         entities.Money `json:"price"`
+	CustomerEmail string         `json:"customer_email"`
+}
+
+func (h Handler) PostTicketsStatus(c echo.Context) error {
+	var request TicketsStatusRequest
 	err := c.Bind(&request)
 	if err != nil {
 		return err
 	}
 
 	for _, ticket := range request.Tickets {
-		msg := message.NewMessage(watermill.NewUUID(), []byte(ticket))
+		msg := message.NewMessage(watermill.NewUUID(), []byte(ticket.TicketID))
 
 		err = h.publisher.Publish("issue-receipt", msg)
 		if err != nil {
 			return err
 		}
 
-		msg = message.NewMessage(watermill.NewUUID(), []byte(ticket))
+		msg = message.NewMessage(watermill.NewUUID(), []byte(ticket.TicketID))
 
 		err = h.publisher.Publish("append-to-tracker", msg)
 		if err != nil {
