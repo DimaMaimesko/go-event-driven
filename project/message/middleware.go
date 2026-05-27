552e52330234a02cd34f3f5134245b78c73064a1
Three Dots Labs <contact@threedotslabs.com>
2026-05-27 23:08:17 +0300

    completed 06-middlewares/03-project-correlation-id

--- project/message/middleware.go
+++ project/message/middleware.go
@@ -3,12 +3,22 @@ package message
 import (
 	"log/slog"
 
+	"github.com/ThreeDotsLabs/go-event-driven/v2/common/log"
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
 )
 
 func useMiddlewares(router *message.Router) {
 	router.AddMiddleware(middleware.Recoverer)
+
+	router.AddMiddleware(func(next message.HandlerFunc) message.HandlerFunc {
+		return func(msg *message.Message) ([]*message.Message, error) {
+			ctx := log.ContextWithCorrelationID(msg.Context(), msg.Metadata.Get("correlation_id"))
+			msg.SetContext(ctx)
+
+			return next(msg)
+		}
+	})
 
 	router.AddMiddleware(func(next message.HandlerFunc) message.HandlerFunc {
 		return func(msg *message.Message) ([]*message.Message, error) {
