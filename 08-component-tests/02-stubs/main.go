abb5be1f96e309f0f54a2f9cedb87c57bd05c045
Three Dots Labs <contact@threedotslabs.com>
2026-05-29 00:05:54 +0300

    completed 08-component-tests/02-stubs

--- 08-component-tests/02-stubs/main.go
+++ 08-component-tests/02-stubs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"sync"
 )
 
 type IssueReceiptRequest struct {
@@ -20,4 +21,14 @@ type ReceiptsService interface {
 
 type ReceiptsServiceStub struct {
 	// todo: implement me
+	lock           sync.Mutex
+	IssuedReceipts []IssueReceiptRequest
+}
+
+func (s *ReceiptsServiceStub) IssueReceipt(ctx context.Context, request IssueReceiptRequest) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.IssuedReceipts = append(s.IssuedReceipts, request)
+	return nil
 }
