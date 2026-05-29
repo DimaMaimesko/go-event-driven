0f3ba2b4be8b817c0ca429613e87eccb44c97e9e
Three Dots Labs <contact@threedotslabs.com>
2026-05-29 16:09:45 +0300

    override with example solution for 09-cqrs-events/05-event-handler

--- 09-cqrs-events/05-event-handler/main.go
+++ 09-cqrs-events/05-event-handler/main.go
@@ -15,11 +15,21 @@ type EventsCounter interface {
 	CountEvent() error
 }
 
+type EventsHandler struct {
+	counter EventsCounter
+}
+
+func (h EventsHandler) Handle(ctx context.Context, event *FollowRequestSent) error {
+	return h.counter.CountEvent()
+}
+
 func NewFollowRequestSentHandler(counter EventsCounter) cqrs.EventHandler {
+	h := EventsHandler{
+		counter: counter,
+	}
+
 	return cqrs.NewEventHandler(
-		"FollowRequestSentHandler",
-		func(ctx context.Context, event *FollowRequestSent) error {
-			return counter.CountEvent()
-		},
+		"FollowRequestSent",
+		h.Handle,
 	)
 }
