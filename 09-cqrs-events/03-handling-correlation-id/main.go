150da9b5bc20b18aea2e32209f24263e9e346487
Three Dots Labs <contact@threedotslabs.com>
2026-05-29 15:28:44 +0300

    completed 09-cqrs-events/03-handling-correlation-id

--- 09-cqrs-events/03-handling-correlation-id/main.go
+++ 09-cqrs-events/03-handling-correlation-id/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
 	"github.com/lithammer/shortuuid/v3"
 )
 
@@ -13,7 +14,10 @@ type CorrelationPublisherDecorator struct {
 
 func (c CorrelationPublisherDecorator) Publish(topic string, messages ...*message.Message) error {
 	// custom logic here
-
+	for _, msg := range messages {
+		correlationID := CorrelationIDFromContext(msg.Context())
+		middleware.SetCorrelationID(correlationID, msg)
+	}
 	return c.Publisher.Publish(topic, messages...)
 }
 
