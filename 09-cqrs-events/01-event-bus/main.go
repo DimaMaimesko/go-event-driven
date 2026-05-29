df9446c3e89035a3fb3b45cb65c825d4fa7b030e
Three Dots Labs <contact@threedotslabs.com>
2026-05-29 14:07:36 +0300

    completed 09-cqrs-events/01-event-bus

--- 09-cqrs-events/01-event-bus/main.go
+++ 09-cqrs-events/01-event-bus/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ThreeDotsLabs/watermill/components/cqrs"
 	"github.com/ThreeDotsLabs/watermill/message"
 )
 
 func NewEventBus(pub message.Publisher) (*cqrs.EventBus, error) {
-	return nil, fmt.Errorf("not implemented")
+	return cqrs.NewEventBusWithConfig(pub, cqrs.EventBusConfig{
+		Marshaler: cqrs.JSONMarshaler{},
+		GeneratePublishTopic: func(params cqrs.GenerateEventPublishTopicParams) (string, error) {
+			return params.EventName, nil
+		},
+	})
 }
