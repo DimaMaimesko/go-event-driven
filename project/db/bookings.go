889b8c537d0251884d4e3e6c14573061f97f5adf
Three Dots Labs <contact@threedotslabs.com>
2026-06-02 23:15:19 +0300

    completed 11-outbox/10-project-emit-place-booked

--- project/db/bookings.go
+++ project/db/bookings.go
@@ -2,7 +2,11 @@ package db
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"tickets/message/event"
+	"tickets/message/outbox"
 
 	"github.com/jmoiron/sqlx"
 
@@ -22,14 +26,64 @@ func NewBookingsRepository(db *sqlx.DB) BookingsRepository {
 }
 
 func (b BookingsRepository) AddBooking(ctx context.Context, booking entities.Booking) (err error) {
-	_, err = b.db.NamedExecContext(ctx, `
-		INSERT INTO 
-		    bookings (booking_id, show_id, number_of_tickets, customer_email) 
-		VALUES (:booking_id, :show_id, :number_of_tickets, :customer_email)
-		`, booking)
+	return updateInTx(
+		ctx,
+		b.db,
+		sql.LevelRepeatableRead,
+		func(ctx context.Context, tx *sqlx.Tx) error {
+			_, err := tx.NamedExecContext(ctx, `
+				INSERT INTO 
+				    bookings (booking_id, show_id, number_of_tickets, customer_email) 
+				VALUES (:booking_id, :show_id, :number_of_tickets, :customer_email)
+				`, booking)
+			if err != nil {
+				return fmt.Errorf("could not add booking: %w", err)
+			}
+
+			outboxPublisher, err := outbox.NewPublisherForDb(ctx, tx)
+			if err != nil {
+				return fmt.Errorf("could not create event bus publisher: %w", err)
+			}
+
+			bus := event.NewBus(outboxPublisher)
+
+			err = bus.Publish(ctx, entities.BookingMade{
+				Header:          entities.NewMessageHeader(),
+				NumberOfTickets: booking.NumberOfTickets,
+				BookingID:       booking.BookingID.String(),
+				CustomerEmail:   booking.CustomerEmail,
+				ShowID:          booking.ShowID.String(),
+			})
+			if err != nil {
+				return fmt.Errorf("could not publish BookingMade event: %w", err)
+			}
+
+			return nil
+		},
+	)
+}
+
+func updateInTx(
+	ctx context.Context,
+	db *sqlx.DB,
+	isolation sql.IsolationLevel,
+	fn func(ctx context.Context, tx *sqlx.Tx) error,
+) (err error) {
+	tx, err := db.BeginTxx(ctx, &sql.TxOptions{Isolation: isolation})
 	if err != nil {
-		return fmt.Errorf("could not add booking: %w", err)
+		return fmt.Errorf("could not begin transaction: %w", err)
 	}
 
-	return nil
+	defer func() {
+		if err != nil {
+			if rollbackErr := tx.Rollback(); rollbackErr != nil {
+				err = errors.Join(err, rollbackErr)
+			}
+			return
+		}
+
+		err = tx.Commit()
+	}()
+
+	return fn(ctx, tx)
 }
--- project/message/outbox/outbox.go
+++ project/message/outbox/outbox.go
@@ -1,3 +1,41 @@
 package outbox
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/ThreeDotsLabs/go-event-driven/v2/common/log"
+	"github.com/ThreeDotsLabs/watermill"
+	watermillSQL "github.com/ThreeDotsLabs/watermill-sql/v3/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/components/forwarder"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/jmoiron/sqlx"
+)
+
 const outboxTopic = "events_to_forward"
+
+func NewPublisherForDb(ctx context.Context, tx *sqlx.Tx) (message.Publisher, error) {
+	logger := watermill.NewSlogLogger(log.FromContext(ctx))
+
+	var publisher message.Publisher
+	publisher, err := watermillSQL.NewPublisher(
+		tx,
+		watermillSQL.PublisherConfig{
+			SchemaAdapter: watermillSQL.DefaultPostgreSQLSchema{},
+		},
+		logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not create publisher: %w", err)
+	}
+
+	publisher = log.CorrelationPublisherDecorator{Publisher: publisher}
+
+	publisher = forwarder.NewPublisher(publisher, forwarder.PublisherConfig{
+		ForwarderTopic: outboxTopic,
+	})
+
+	publisher = log.CorrelationPublisherDecorator{Publisher: publisher}
+
+	return publisher, nil
+}
