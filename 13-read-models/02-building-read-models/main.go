12293b1f77bafbb5e944f9bdcd544a16e5244795
Three Dots Labs <contact@threedotslabs.com>
2026-06-03 23:08:59 +0300

    completed 13-read-models/02-building-read-models

--- 13-read-models/02-building-read-models/main.go
+++ 13-read-models/02-building-read-models/main.go
@@ -70,18 +70,47 @@ func (s *InvoiceReadModelStorage) InvoiceByID(id string) (InvoiceReadModel, bool
 }
 
 func (s *InvoiceReadModelStorage) OnInvoiceIssued(ctx context.Context, event *InvoiceIssued) error {
-	// TODO: implement
-	return fmt.Errorf("not implemented")
+	invoice, exist := s.InvoiceByID(event.InvoiceID)
+	if !exist {
+		invoice = InvoiceReadModel{}
+	}
+	invoice.InvoiceID = event.InvoiceID
+	invoice.CustomerName = event.CustomerName
+	invoice.Amount = event.Amount
+	invoice.IssuedAt = event.IssuedAt
+
+	s.invoices[event.InvoiceID] = invoice
+
+	return nil
 }
 
 func (s *InvoiceReadModelStorage) OnInvoicePaymentReceived(ctx context.Context, event *InvoicePaymentReceived) error {
-	// TODO: implement
-	return fmt.Errorf("not implemented")
+	invoice, exist := s.InvoiceByID(event.InvoiceID)
+	if !exist {
+		return fmt.Errorf("invoice %s not found", event.InvoiceID)
+	}
+
+	invoice.PaidAmount = invoice.PaidAmount.Add(event.PaidAmount)
+	invoice.LastPaymentAt = event.PaidAt
+	invoice.FullyPaid = event.FullyPaid
+
+	s.invoices[event.InvoiceID] = invoice
+
+	return nil
 }
 
 func (s *InvoiceReadModelStorage) OnInvoiceVoided(ctx context.Context, event *InvoiceVoided) error {
-	// TODO: implement
-	return fmt.Errorf("not implemented")
+	invoice, exist := s.InvoiceByID(event.InvoiceID)
+	if !exist {
+		return fmt.Errorf("invoice %s not found", event.InvoiceID)
+	}
+
+	invoice.Voided = true
+	invoice.VoidedAt = event.VoidedAt
+
+	s.invoices[event.InvoiceID] = invoice
+
+	return nil
 }
 
 func NewRouter(storage *InvoiceReadModelStorage, eventProcessorConfig cqrs.EventProcessorConfig, watermillLogger watermill.LoggerAdapter) (*message.Router, error) {
@@ -93,7 +122,19 @@ func NewRouter(storage *InvoiceReadModelStorage, eventProcessorConfig cqrs.Event
 	}
 
 	err = eventProcessor.AddHandlers(
-	// TODO: add event handlers
+		// TODO: add event handlers
+		cqrs.NewEventHandler(
+			"InvoiceIssued",
+			storage.OnInvoiceIssued,
+		),
+		cqrs.NewEventHandler(
+			"OnInvoicePaymentReceived",
+			storage.OnInvoicePaymentReceived,
+		),
+		cqrs.NewEventHandler(
+			"OnInvoiceVoided",
+			storage.OnInvoiceVoided,
+		),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("could not add event handlers: %w", err)
